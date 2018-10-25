9e242fb9853b4837658e17a13142160bf61cc9a5
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-10-25 16:41:41 -0600

    fixes small error created by controller migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'register', to: 'user#new', as: "register"
+  get 'register', to: 'users#new', as: "register"
   get 'login', to: 'sessions#new', as: "login"
   get 'logout', to: 'sessions#destroy', as: "logout"
 
-  
+
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
-  
+
   get '/profile/:id', to: 'users#show', as: 'profile'
-  
+
   get '/profile/edit/:id', to: 'users#edit', as: 'edit_profile'
   patch '/profile/edit/:id', to: 'users#update'
   
