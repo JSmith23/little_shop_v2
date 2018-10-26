8e76d1239ef0103fcc0b05a17ab9bafee1d3df41
Michael Gatewood <michael@mngatewood.com>
2018-10-25 18:16:52 -0600

    Fixes user views to user current_user

--- spec/features/user_updates_profile_spec.rb
+++ spec/features/user_updates_profile_spec.rb
@@ -4,15 +4,16 @@ describe 'As a registered user, merchant, or admin' do
 
   before(:each) do
     @user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
 
   describe 'when I visit my own profile page' do
     it 'should display a form to update my profile data' do
 
-      visit edit_profile_path(@user)
+      visit profile_edit_path
 
       within("main.update-user") do
-        expect(current_path).to eq(edit_profile_path(@user))
+        expect(current_path).to eq(profile_edit_path)
         expect(page).to have_content("Update Profile Information")
       end
 
@@ -22,10 +23,10 @@ describe 'As a registered user, merchant, or admin' do
   describe 'when I visit my own profile page' do
     it 'should display a form to update my profile data' do
 
-      visit edit_profile_path(@user)
+      visit profile_edit_path
 
       within("main.update-user") do
-        expect(current_path).to eq(edit_profile_path(@user))
+        expect(current_path).to eq(profile_edit_path)
         expect(page).to have_content("Update Profile Information")
       end
 
