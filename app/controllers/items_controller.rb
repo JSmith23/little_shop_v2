9eb5c0bab4cfab53843c598abcd9212fa60cbd5f
Michael Gatewood <michael@mngatewood.com>
2018-10-26 20:22:20 -0600

    Adds new and edit methods to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController 
-    def index
-        @items = Item.where(enabled:true)
-    end 
+	def index
+		if !current_user
+			@items = Item.where(enabled: true)
+		elsif current_user.role = 'merchant'
+			@items = Item.where(user_id: current_user.id)
+		end
+	end
+	
+	def edit
+		@item = Item.find(params[:id])
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+
+	end
+
 end 
