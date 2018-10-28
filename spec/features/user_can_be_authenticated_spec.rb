1425a24a0929dac99f60ebe64c4c372b10165047
Michael Gatewood <michael@mngatewood.com>
2018-10-28 17:20:55 -0600

    Adds logged in user visits login route redirected to profile assertion

--- spec/features/user_can_be_authenticated_spec.rb
+++ spec/features/user_can_be_authenticated_spec.rb
@@ -17,7 +17,6 @@ describe 'user can create account' do
     fill_in :user_password, with: "test"
     fill_in :user_password_confirmation, with: "test"
 
-
     click_on "Create User"
 
     expect(current_path).to eq(profile_path(User.last))
@@ -80,5 +79,27 @@ describe 'user can create account' do
     click_on "Log Out"
 
     expect(current_path).to eq(root_path)
+  end
+
+  it 'logged in user visiting login page is redirected to profile' do
+      
+    user = User.create!(name: "Test Person", address: "123 lane", city: "Denver", state: "CO", zip: "80205", email: "test@test.com", password: "12345", password_confirmation: "12345")
+
+    visit root_path
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :email, with: "test@test.com"
+    fill_in :password, with: "12345"
+
+    click_on "Log in"
+
+    expect(page).to have_content("Logged in as #{user.name}")
+
+    visit login_path
+
+    expect(current_path).to eq(profile_path)
   end
 end
