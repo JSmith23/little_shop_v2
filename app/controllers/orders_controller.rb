4dca2f123911f57f4535f0df29c5364b848a03ce
Michael Gatewood <michael@mngatewood.com>
2018-10-27 21:30:59 -0600

    Adds destroy method to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,19 @@ class OrdersController < ApplicationController
     if current_user.role == 'registered_user'
       @user = current_user
       @orders = Order.where(user_id: @user.id)
+      @heading = "Orders for #{@user.name}"
     elsif current_user.role == 'merchant'
       @user = current_user
-      @greeting = "Merchant Orders for #{@user.name}"
       @orders = Order.joins(:items).where(items: {user_id: @user.id})
+      @heading = "Merchant Orders for #{@user.name}"
     else
       redirect_to login_path
     end
-	end
+  end
+  
+  def destroy
+    order = Order.find(params[:id])
+    order.cancel
+    redirect_back(fallback_location: root_path)
+  end
 end
