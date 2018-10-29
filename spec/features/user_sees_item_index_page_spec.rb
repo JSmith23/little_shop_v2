109309a75fd5424ebfb3563647687f3ce2bf0e7c
Michael Gatewood <michael@mngatewood.com>
2018-10-29 14:17:09 -0600

    Adds can click on name to view item show

--- spec/features/user_sees_item_index_page_spec.rb
+++ spec/features/user_sees_item_index_page_spec.rb
@@ -1,18 +1,37 @@
 require "rails_helper"
 
-describe "user sees items" do
+describe "As a user" do
+
+  before(:each) do
+    @user = create(:user)
+    @item_1 = @user.items.create!(name:"Item_1", thumbnail:"http://s7d2.scene7.com/is/image/JCPenney/DP0625201317031085M.tif?wid=65&hei=65&fmt=jpg&op_usm=.4,.8,0,0&resmode=sharp2", price: 1, inventory: 1, description:"This is item 1", enabled: true )
+    @item_2 = @user.items.create!(name:"Item_2", thumbnail:"http://s7d2.scene7.com/is/image/JCPenney/DP0625201317031085M.tif?wid=65&hei=65&fmt=jpg&op_usm=.4,.8,0,0&resmode=sharp2", price: 2, inventory: 2, description:"This is item 2", enabled: false )
+  end
+
+  describe "when I visit the items index page" do
     it "user sees items basic attributes" do
-        user = create(:user)
-        item_1 = Item.create!(name:"Item_1", thumbnail:"http://s7d2.scene7.com/is/image/JCPenney/DP0625201317031085M.tif?wid=65&hei=65&fmt=jpg&op_usm=.4,.8,0,0&resmode=sharp2", price: 1, inventory: 1, description:"This is item 1", enabled: true, user_id: user.id)
-        item_2 = Item.create!(name:"Item_2", thumbnail:"http://s7d2.scene7.com/is/image/JCPenney/DP0625201317031085M.tif?wid=65&hei=65&fmt=jpg&op_usm=.4,.8,0,0&resmode=sharp2", price: 2, inventory: 2, description:"This is item 2", enabled: false, user_id: user.id)
+      visit items_path
 
-        visit items_path
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_1.id)
+      expect(page).to have_content(@item_1.price)
+      expect(page).to have_content(@item_1.inventory)
+      expect(page).to_not have_content(@item_2.name)
+    end
+  end
 
-        expect(page).to have_content(item_1.name)
-        expect(page).to have_content(item_1.id)
-        expect(page).to have_content(item_1.price)
-        expect(page).to have_content(item_1.inventory)
-        expect(page).to_not have_content(item_2.name)
+  describe "when I click the name of an item" do
+    it "displays the item show page" do
+      visit items_path
+
+      click_on @item_1.name
+
+      expect(current_path).to eq(item_path(@item_1))
+      expect(page).to have_content(@item_1.description)
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@item_1.inventory)
+      expect(page).to have_content(@item_1.price)
 
     end
+  end
 end
