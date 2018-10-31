00563fb20c84817a5a38f7bc003987069d207e07
Michael Gatewood <michael@mngatewood.com>
2018-10-31 13:57:11 -0600

    Removes unused method

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,8 +2,4 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  def self.merchant_order_items(order_id, merchant_id)
-    joins(:items).where('items.user_id = #{user}, orders.id = #{order_id}')
-  end
-
 end
