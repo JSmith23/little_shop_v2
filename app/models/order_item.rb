d44445ca3374bb3bbfd3b9cc537a140a7fd6fefe
Michael Gatewood <michael@mngatewood.com>
2018-10-31 11:23:24 -0600

    Adds merchant order items method to model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,9 @@
 class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  def self.merchant_order_items(order_id, merchant_id)
+    joins(:items).where('items.user_id = #{user}, orders.id = #{order_id}')
+  end
+
 end
