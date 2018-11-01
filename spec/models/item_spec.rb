4e3940a6fdb17c6324ed3b96e1d438642660468a
Michael Gatewood <michael@mngatewood.com>
2018-11-01 01:40:02 -0600

    Adds reduce inventory method to item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,4 +15,14 @@ describe Item, type: :model do
     it { should have_many(:orders).through(:order_items)}
     it { should have_many(:order_items)}
   end
+
+  describe "Methods" do
+    it 'should reduce the inventory by the argument' do
+      @merchant = create(:user, :merchant)
+      @item = Item.create( name: "Oval Link Chain Belt",	description: "Add something a little unexpected with this oval link chain belt that takes your look from casual to chic in an instant.", price:	36.26, thumbnail: "http://s7d2.scene7.com/is/image/JCPenney/DP0625201317031085M.tif?wid=65&hei=65&fmt=jpg&op_usm=.4,.8,0,0&resmode=sharp2", enabled: true, inventory: 100, user_id: @merchant.id )
+      @item.reduce_inventory(25)
+
+      expect(@item.inventory).to eq(75)
+    end
+  end
 end
